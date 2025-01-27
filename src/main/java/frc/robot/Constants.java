fdeff095c81e5ee2cc53baaae13ddc0d013e2b0c
Adan Silva <adan.t.silva77@gmail.com>
2025-01-27 10:05:35 -0800

    Create actual project + install java formatter

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,32 +1,28 @@
 package frc.robot;
 
 public class Constants {
-    
-    public static class ElevatorConstants {
-        
-        public static final int elevatorMotorRt = 13;
-        public static final int elevatorMotorLt = 14;
 
-        public static final double kP = 0.001;
-        public static final double kI = 0;
-        public static final double kD = 0;
-        
-    }
+  public static class ElevatorConstants {
 
-    public static class ClimberConstants {
-        
-        public static final int climbMotor1 = 10;
-        public static final int climbMotor2 = 11;
-        
-    }
-    
-    public static class IntakeConstants {
+    public static final int elevatorMotorRt = 13;
+    public static final int elevatorMotorLt = 14;
+
+    public static final double kP = 0.001;
+    public static final double kI = 0;
+    public static final double kD = 0;
+  }
+
+  public static class ClimberConstants {
+
+    public static final int climbMotor1 = 10;
+    public static final int climbMotor2 = 11;
+  }
+
+  public static class IntakeConstants {
 
     public final int floorAlgaeRollerMotor = 15;
     public final int floorAlgaePivotMotor = 16;
     public final int reefAlgaePivotMotor = 17;
     public final int coralRollerMotor = 18;
-
-    }
-
+  }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,37 +2,34 @@ package frc.robot;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-
-import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Swerve;
 
 @SuppressWarnings("unused")
 public class RobotContainer {
-    
-  private double MaxSpeed = DriveConstants.MaxSpeed * 0.5; // 0.1 is about 0.5 mps, 0.7 / 90% is max, go no higher
-                                                           // Value should be tuned with new 2025 code
 
-  private double MaxAngularRate = DriveConstants.MaxAngularRate; // Controls how fast the robot quick turns
-  
-  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-    .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.07) // Add a 7% deadband
-    .withDriveRequestType(DriveRequestType.Velocity); 
+  private double MaxSpeed =
+      DriveConstants.MaxSpeed * 0.5; // 0.1 is about 0.5 mps, 0.7 / 90% is max, go no higher
+  // Value should be tuned with new 2025 code
+
+  private double MaxAngularRate =
+      DriveConstants.MaxAngularRate; // Controls how fast the robot quick turns
+
+  private final SwerveRequest.FieldCentric drive =
+      new SwerveRequest.FieldCentric()
+          .withDeadband(MaxSpeed * 0.1)
+          .withRotationalDeadband(MaxAngularRate * 0.07) // Add a 7% deadband
+          .withDriveRequestType(DriveRequestType.Velocity);
 
   private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
   private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
@@ -48,37 +45,39 @@ public class RobotContainer {
   private final CommandXboxController controller = new CommandXboxController(0);
   private final CommandJoystick appendageJoystick = new CommandJoystick(1);
 
-    PIDController xPID = new PIDController(3, 0, 0);
-    PIDController yPID = new PIDController(3, 0, 0);
-    PIDController rPID = new PIDController(3, 0, 0);
+  PIDController xPID = new PIDController(3, 0, 0);
+  PIDController yPID = new PIDController(3, 0, 0);
+  PIDController rPID = new PIDController(3, 0, 0);
 
   public boolean isRed() {
 
     if (DriverStation.getAlliance().isPresent()) {
 
-      return DriverStation.getAlliance().get() ==  Alliance.Red;
-
+      return DriverStation.getAlliance().get() == Alliance.Red;
     }
 
     return false;
-
-    }
+  }
 
   public RobotContainer() {
 
     configureBindings();
     configureAxisActions();
-
   }
 
   private void configureBindings() {
 
     // Bindings for the controller
     controller.a().whileTrue(drivetrain.applyRequest(() -> brake));
-    controller.b().whileTrue(drivetrain.applyRequest(() -> 
-    point.withModuleDirection(new Rotation2d(-controller.getLeftY(), -controller.getLeftX()))));
+    controller
+        .b()
+        .whileTrue(
+            drivetrain.applyRequest(
+                () ->
+                    point.withModuleDirection(
+                        new Rotation2d(-controller.getLeftY(), -controller.getLeftX()))));
     controller.x().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
-    
+
     drivetrain.registerTelemetry(logger::telemeterize);
 
     // Bindings for the appendage joystick
@@ -99,43 +98,48 @@ public class RobotContainer {
     // appendageJoystick.button(11).onTrue(new InstantCommand(() -> climber.climbDown()));
     // appendageJoystick.button(11).onFalse(new InstantCommand(() -> climber.climbStop()));
 
-   // Intake control
+    // Intake control
     // appendageJoystick.button(2).onTrue(new InstantCommand(() -> intake.floorAlgaeIntake()));
     // appendageJoystick.button(2).onFalse(new InstantCommand(() -> intake.floorAlgaeStop()));
 
     // appendageJoystick.button(3).onTrue(new InstantCommand(() -> intake.floorAlgaeOuttake()));
     // appendageJoystick.button(3).onFalse(new InstantCommand(() -> intake.floorAlgaeStop()));
-    
+
     // appendageJoystick.button(5).onTrue(new InstantCommand(() -> intake.reefAlgaeIntake()));
     // appendageJoystick.button(5).onFalse(new InstantCommand(() -> intake.reefAlgaeStop()));
 
     // appendageJoystick.button(6).onTrue(new InstantCommand(() -> intake.reefAlgaeOuttake()));
-    // appendageJoystick.button(6).onFalse(new InstantCommand(() -> intake.reefAlgaeStop()));    
+    // appendageJoystick.button(6).onFalse(new InstantCommand(() -> intake.reefAlgaeStop()));
 
   }
-  
+
   private void configureAxisActions() {
-    
+
     // drivetrain.setDefaultCommand(
     //   new RunCommand(() -> {
-    //   // x, y, rotation, doaim (button), PhotonVision camera 
-    //   drivetrain.drive(() -> joystick.getLeftX(), 
-    //     () -> joystick.getLeftY(), 
-    //     () -> joystick.getRightX(), 
+    //   // x, y, rotation, doaim (button), PhotonVision camera
+    //   drivetrain.drive(() -> joystick.getLeftX(),
+    //     () -> joystick.getLeftY(),
+    //     () -> joystick.getRightX(),
     //     () -> joystick.leftBumper().getAsBoolean(), camera);
     //   }));
 
-     drivetrain.setDefaultCommand(
-      drivetrain.applyRequest(() -> drive.withVelocityX(-controller.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-            .withVelocityY(-controller.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-            .withRotationalRate(-controller.getRightX() * MaxAngularRate))); // Drive counterclockwise with negative X (left)
-
+    drivetrain.setDefaultCommand(
+        drivetrain.applyRequest(
+            () ->
+                drive
+                    .withVelocityX(
+                        -controller.getLeftY()
+                            * MaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(
+                        -controller.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(
+                        -controller.getRightX()
+                            * MaxAngularRate))); // Drive counterclockwise with negative X (left)
   }
 
   public Command getAutonomousCommand() {
 
     return null;
-
   }
-
 }
