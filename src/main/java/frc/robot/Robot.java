817896d1a38d0075e1d5608fccebe03b973636b9
MrFurbie <jayjayrebruf@gmail.com>
2025-02-18 17:32:24 -0800

    Added LEDs

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,27 +1,23 @@
 package frc.robot;
 
-import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
-import com.ctre.phoenix6.swerve.SwerveRequest;
-
-import choreo.auto.AutoChooser;
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
-import edu.wpi.first.math.controller.PIDController;
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.lib.util.Telemetry;
 // import frc.robot.subsystems.Climber;
 // import frc.robot.subsystems.Elevator;
 // import frc.robot.subsystems.Groundtake;
 // import frc.robot.subsystems.Reeftake;
 import frc.robot.subsystems.Swerve;
+
 // import frc.robot.subsystems.Vision;
 
 public class Robot extends TimedRobot {
@@ -29,7 +25,6 @@ public class Robot extends TimedRobot {
   private double MaxSpeed = DriveConstants.MaxSpeed;
   private double MaxAngularRate = DriveConstants.MaxAngularRate;
   private SendableChooser<AutoRoutine> autoChooser = new SendableChooser<>();
-  
 
   private final SwerveRequest.FieldCentric drive =
       new SwerveRequest.FieldCentric()
@@ -56,7 +51,7 @@ public class Robot extends TimedRobot {
 
   public Robot() {
 
-    // Create Choreo 
+    // Create Choreo
     autoFactory =
         new AutoFactory(
             () -> drivetrain.getState().Pose,
@@ -65,20 +60,19 @@ public class Robot extends TimedRobot {
             true,
             drivetrain);
 
-            autoChooser.addOption("Forward", testAuto());
-            autoChooser.addOption("Orbit Reef", testAuto());
+    autoChooser.addOption("Forward", testAuto());
+    autoChooser.addOption("Orbit Reef", testAuto());
 
-  // Controler Bindings 
-  drivetrain.setDefaultCommand(
-    drivetrain.applyRequest(
-        () ->
-            drive
-                .withVelocityX(-controller.getLeftY() * MaxSpeed)
-                .withVelocityY(-controller.getLeftX() * MaxSpeed)
-                .withRotationalRate(-controller.getRightX() * MaxAngularRate)));
+    // Controler Bindings
+    drivetrain.setDefaultCommand(
+        drivetrain.applyRequest(
+            () ->
+                drive
+                    .withVelocityX(-controller.getLeftY() * MaxSpeed)
+                    .withVelocityY(-controller.getLeftX() * MaxSpeed)
+                    .withRotationalRate(-controller.getRightX() * MaxAngularRate)));
 
-    
-        // Bindings for the controller
+    // Bindings for the controller
     controller.a().whileTrue(drivetrain.applyRequest(() -> brake));
     controller.x().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
@@ -109,8 +103,6 @@ public class Robot extends TimedRobot {
     // appendageJoystick.button(7).onTrue(new InstantCommand(() -> elevator.a2()));
 
     SmartDashboard.putData("Auto Chooser", autoChooser);
-    
-
   }
 
   @Override
@@ -122,7 +114,6 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
-    
   }
 
   @Override
@@ -134,8 +125,8 @@ public class Robot extends TimedRobot {
     AutoTrajectory testTrajectory = routine.trajectory("Forward");
 
     routine
-      .active()
-      .onTrue(Commands.sequence(testTrajectory.resetOdometry(), testTrajectory.cmd()));
+        .active()
+        .onTrue(Commands.sequence(testTrajectory.resetOdometry(), testTrajectory.cmd()));
 
     return routine;
   }
@@ -146,10 +137,9 @@ public class Robot extends TimedRobot {
     AutoTrajectory testTrajectory = routine.trajectory("Orbit Reef");
 
     routine
-      .active()
-      .onTrue(Commands.sequence(testTrajectory.resetOdometry(), testTrajectory.cmd()));
+        .active()
+        .onTrue(Commands.sequence(testTrajectory.resetOdometry(), testTrajectory.cmd()));
 
     return routine;
   }
-
 }
--- src/main/java/frc/robot/subsystems/Reeftake.java
+++ src/main/java/frc/robot/subsystems/Reeftake.java
@@ -10,7 +10,8 @@
 
 // public class Reeftake extends ProfiledPIDSubsystem {
 
-//   private final TalonFX reefAlgaePivotMotor = new TalonFX(Constants.ReeftakeConstants.pivotMotorID);
+//   private final TalonFX reefAlgaePivotMotor = new
+// TalonFX(Constants.ReeftakeConstants.pivotMotorID);
 //   private final TalonFX intakeMotor = new TalonFX(Constants.ReeftakeConstants.intakeMotorID);
 
 //   private final DigitalInput hasCoral =
