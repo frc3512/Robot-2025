db8553aaf7458e4ef44e76a2a2cebefc07152d71
MrFurbie <jayjayrebruf@gmail.com>
2025-03-22 08:12:41 -0800

    more vision

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -35,8 +35,8 @@ public class Constants {
     public static final double l2Pos = 14.2;
     public static final double l3Pos = 26;
     public static final double l4Pos = 46;
-    public static final double a1Pos = 9.5;
-    public static final double a2Pos = 21.84;
+    public static final double a1Pos = 3.5;
+    public static final double a2Pos = 15.84;
   }
 
   public static class ClimberConstants {
--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -252,7 +252,7 @@ public class Swerve extends TunerSwerveDrivetrain implements Subsystem {
   public Command goToPose(Supplier<Pose2d> target) {
     return this.run(() -> controlPosition(target.get()))
         .until(() -> false)
-        .andThen(Commands.runOnce(() -> this.applyRequest(new SwerveRequest.FieldCentric())));
+        .andThen(Commands.runOnce(() -> this.applyRequest(new SwerveRequest.RobotCentric())));
   }
 
   @Override
